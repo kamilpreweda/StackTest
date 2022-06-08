bde81f33022c8028e5f19497c0d1d82f80a23522
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-08 09:32:13 +0200

    Added few examples

--- StackTest/Program.cs
+++ StackTest/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("pierwszy na stosie");
+            myStack.Push("drugi na stosie");
+            myStack.Push("trzeci na stosie");
+            myStack.Push("ostatni na stosie");
+
+            // Metoda Peek działą dla stosu tak samo jak dla kolejki.
+            Console.WriteLine($"Metoda Peek() zwróciła: \n{myStack.Peek()}");
+
+            // Metoda Pop pobiera następny element z WIERZCHOŁKA stosu.
+            Console.WriteLine($"Pierwsze wywołanie Pop() zwróciło: \n{myStack.Pop()}");
+            Console.WriteLine($"Drugie wywołanie Pop() zwróciło: \n{myStack.Pop()}");
+
+            Console.WriteLine($"Count przed wywołaniem Clear(): \n{myStack.Count()}");
+            myStack.Clear();
+            Console.WriteLine($"Count po wywołaniu Clear(): \n{myStack.Count()}");
         }
+
+        
+
+        
     }
 }
